bba4c71b1e6e9f37118c556343779694636b7b2a
Hoang Nguyen <hoang.nguyen@xing.com>
2017-11-15 11:35:02 +0100

    Refactor read_yaml method

---
+++ lib/honitor/user_config.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'virtus'
+
+class UserConfig
+  include Virtus.model
+
+  attribute :name, String
+  attribute :link, String
+  attribute :dom_class, String
+  attribute :interval, Integer, default: 30
+  attribute :random, Boolean, default:   true
+end
