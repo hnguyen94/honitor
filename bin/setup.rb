3f28b82c266bc1ca357ca1ca501a59dd556cdeda
Nicolas Heinig <nicolas.heinig@xing.com>
2017-11-15 09:49:50 +0100

    Rm unneccessary require

--- bin/setup.rb
+++ bin/setup.rb
@@ -3,8 +3,6 @@
 require 'colorize'
 require 'fileutils'
 require 'yaml'
-require 'uri'
-
 include FileUtils
 
 LINK_REGEXP = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
