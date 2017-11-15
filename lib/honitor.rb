b9b534d3b7e95d984dac6980988ceece1b26240e
Hoang Nguyen <hoang.nguyen@xing.com>
2017-11-15 17:32:51 +0100

    Refactor and add helper methods

--- lib/honitor.rb
+++ lib/honitor.rb
@@ -6,38 +6,41 @@ class Honitor
       read_config
       switch_log(@user_config.log)
       mkdir 'logs'
-      check_changes(time_interval: @user_config.interval, random: @user_config.random)
+      check_changes
     end
 
     def prepare_message
       if no_changes?
-        puts show_time.to_s.green + " Found #{@current_dom_objects.count} items || " + 'No changes'.yellow
+        message = " Found #{@current_dom_objects.count} items || " + 'No changes'.yellow
       else
+        message = " Found new #{new_changes.count} items || " + 'Sent message!'.blue
         new_changes = @current_dom_objects - @old_dom_objects
-
-        puts show_time.to_s.green + " Found new #{new_changes.count} items || " + 'Sent message!'.blue
         PushoverApi.send_push_notification(
           message: "On #{@user_config.name} there are #{new_changes.count} updated items."
         )
       end
+
+      puts HonitorHelpers::Log.show_time.to_s.green + message
     end
 
-    def check_changes(time_interval:, random:)
+    def check_changes
       loop do
         page = MechanizeBot.new(link: @user_config.link, dom_class: @user_config.dom_class)
-        @current_dom_objects = beautify(xml_array: page.fetch_dom_objects)
+        @current_dom_objects = HonitorHelpers::Mechanize.beautify(xml_list: page.fetch_dom_objects)
 
         prepare_message
 
         @old_dom_objects = @current_dom_objects
 
-        sleep time_interval unless random
-        sleep(rand(1...time_interval))
+        sleep @user_config.interval unless @user_config.random
+        sleep(rand(1...@user_config.interval))
       end
     end
 
     def no_changes?
-      @old_dom_objects.nil? || @old_dom_objects == @current_dom_objects
+      @no_changes ||= begin
+        @old_dom_objects.nil? || @old_dom_objects == @current_dom_objects
+      end
     end
 
     def read_config
@@ -56,16 +59,8 @@ class Honitor
       puts 'Log is turned off'
     end
 
-    def beautify(xml_array:)
-      xml_array.map(&:content)
-    end
-
     def mkdir(name)
       FileUtils.mkdir_p(name) unless File.directory?(name)
-    end
-
-    def show_time
-      Time.new.strftime('%a,%e %b %Y %H:%M:%S %z')
     end
   end
 end
