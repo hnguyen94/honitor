41b4d0463507c3706f0b253fdbf1df15ba78ab05
Hoang Nguyen <hoang.nguyen@xing.com>
2018-05-24 10:25:15 +0200

    Add dynamic processes

--- lib/hydra.rb
+++ lib/hydra.rb
@@ -3,10 +3,12 @@ require 'parallel'
 class Hydra
   class << self
     def execute
-      Parallel.map(instances, in_processes: 2) do |instance|
+      Parallel.map(instances, in_processes: instances.count) do |instance|
         Honitor.new(instance: instance)
       end
     end
+
+    private
 
     def instances
       filenames = Dir.entries('configs').reject { |f| File.directory? f }
